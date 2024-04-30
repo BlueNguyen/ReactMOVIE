859f4c4fb84e6439788fa023d43ce6dc772eedf8
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-04-30 18:39:30 +0700

    modify file

---
+++ src/pages/TicketBookingPage/index.jsx
@@ -0,0 +1,50 @@
+import  useScrollToTop  from "../../hooks/useScrollToTop";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Material UI
+import { Box, Container, Grid } from "@mui/material";
+
+// Components
+import TicketBookingCard from "./TicketBookingCard";
+import SeatSelector from "./SeatSelector";
+import Modal from "../../components/Modal";
+
+// Redux actions
+import {fetchTicketBookingDetails , closeModal } from "../../redux/slice/ticketBooking";
+
+// Scss
+import "./style.scss";
+import Header from "../../layout/Header/Header";
+
+const TicketBookingPage = () => {
+  useScrollToTop();
+  const { scheduleId } = useParams();
+  const dispatch = useDispatch();
+  // const modalProps = useSelector((rootReducer) => rootReducer.ticketBooking.modal);
+  const { modal  } = useSelector((state) => state.ticketBookingSlice);
+  useEffect(() => {
+    dispatch(fetchTicketBookingDetails(scheduleId));
+  }, []);
+
+  return (
+    <div className="container ticket-booking-page " component="section">
+      <Header/>
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid item md={8.5} xs={12}>
+            <SeatSelector />
+          </Grid>
+          <Grid item md={3.5} xs={12}>
+            <TicketBookingCard />
+          </Grid>
+        </Grid>
+        <Modal actCloseModal={closeModal} modalProps={modal} />
+      </Container>
+    </div>
+    
+  );
+};
+
+export default TicketBookingPage;
